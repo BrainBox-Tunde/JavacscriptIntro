a4b85f8cdcd63bbc308f565a3d0c266973911362
Tunde BrainBox <realtundeonline@gmail.com>
2022-06-28 13:33:50 +0100

    Adjusted Codes

---
+++ script.js
@@ -0,0 +1,9 @@
+let first_name = prompt("Enter First Name:");
+let last_name = prompt("Enter Last Name:");
+let age = prompt("Enter Age:");
+let height = prompt("Enter height:");
+
+let text = "First name is: " + first_name + ";  " + "Last name is: " + last_name + ";  " +"Age is: "  + age +";  " 
++"Height is: " + height;
+
+alert(text);
